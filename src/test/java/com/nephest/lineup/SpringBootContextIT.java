b9098eedb81a0f84c8e205a47b711c3a9396f4cd
Oleksandr Masniuk <nephestdev@gmail.com>
2022-08-03 12:07:55 +0300

    add Spring Boot context IT

---
+++ src/test/java/com/nephest/lineup/SpringBootContextIT.java
@@ -0,0 +1,16 @@
+//  Copyright (C) 2022 Oleksandr Masniuk
+//  SPDX-License-Identifier: AGPL-3.0-or-later
+
+package com.nephest.lineup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootContextIT {
+
+  @Test
+  public void whenSpringBootIsLoaded_thenNoExceptionIsThrown() {
+  }
+
+}
