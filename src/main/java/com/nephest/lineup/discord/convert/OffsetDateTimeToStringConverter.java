59fc04a485537e520f76cfdadfab52a93523e976
Oleksandr Masniuk <nephestdev@gmail.com>
2022-08-19 10:15:09 +0300

    use Discord timestamp format

--- src/main/java/com/nephest/lineup/discord/convert/OffsetDateTimeToStringConverter.java
+++ src/main/java/com/nephest/lineup/discord/convert/OffsetDateTimeToStringConverter.java
@@ -4,13 +4,12 @@
 package com.nephest.lineup.discord.convert;
 
 import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.NonNull;
 
 public class OffsetDateTimeToStringConverter implements Converter<OffsetDateTime, String> {
   @Override
   public String convert(@NonNull OffsetDateTime source) {
-    return source.withOffsetSameInstant(ZoneOffset.UTC).toString();
+    return "<t:" + source.toEpochSecond() + ">";
   }
 }
